6a680495c2532929a1d90ac2e964e1966d0f94ca
NBobrova <nastyabobr@yandex.ru>
2020-02-11 13:01:51 +0300

    Updates

---
+++ precache-manifest.7c6027bd9a06829afa2d885adcb470e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec59c8d138d055e7af72904c8292b4e",
+    "url": "/todo-react-dnd/index.html"
+  },
+  {
+    "revision": "95202fba0a8cd016655d",
+    "url": "/todo-react-dnd/static/css/main.7487804a.chunk.css"
+  },
+  {
+    "revision": "2d12176e1d2a9741002b",
+    "url": "/todo-react-dnd/static/js/2.2e9161b6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/todo-react-dnd/static/js/2.2e9161b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95202fba0a8cd016655d",
+    "url": "/todo-react-dnd/static/js/main.11bcf0fd.chunk.js"
+  },
+  {
+    "revision": "8444e534a0ef6a3ac4fd",
+    "url": "/todo-react-dnd/static/js/runtime-main.e14659e8.js"
+  }
+]);
